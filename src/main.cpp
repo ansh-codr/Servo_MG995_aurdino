2df40f97de40fc8825d256c70a84b866d77f6aa4
ansh-codr <anshloverao@gmail.com>
2026-02-04 13:43:09 +0530

    Update servo control sequence in loop function

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,14 @@ void setup() {
 }
 
 void loop() {
-    myServo.write(10);
+    myServo.write(0);
     delay(500);
+    myServo.write(90);
+    delay(500);
+    myServo.write(120);
+    delay(500);
+    myServo.write(180);
+    delay(500);
+}
 // write your code here
 }
