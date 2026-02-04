160a76c4a6b1d716e4df44be64974243a3ed0401
ansh-codr <anshloverao@gmail.com>
2026-02-04 13:58:11 +0530

    Refactor servo control in loop function to use incremental angles and add debug output

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,25 @@ void setup() {
 }
 
 void loop() {
-    myServo.write(0);
-    delay(500);
-    myServo.write(90);
-    delay(500);
-    myServo.write(120);
-    delay(500);
-    myServo.write(180);
-    delay(500);
+    // myServo.write(0);
+    // delay(500);
+    // myServo.write(90);
+    // delay(500);
+    // myServo.write(120);
+    // delay(500);
+    // myServo.write(180);
+    // delay(500);
+    for (int angle = 0;angle<=180;angle+=10){
+        myServo.write(angle);
+        Serial.print("Angle: ");
+        Serial.println(angle);
+        delay(500);
 }
-// write your code here
+    // rotates back from 180 to 0
+    for (int angle = 180;angle>=0;angle-=10) {
+        myServo.write(angle);
+        Serial.print("Angle: ");
+        Serial.println(angle);
+        delay(500);
+    }
 }
