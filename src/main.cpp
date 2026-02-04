0adfa22a8a636a8a88c1ed40af93268d92166926
ansh-codr <anshloverao@gmail.com>
2026-02-04 13:38:59 +0530

    Add servo initialization and serial communication setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <Servo.h>
+Servo myServo;
 void setup() {
 // write your initialization code here
+    Serial.begin(9600);
+    myServo.attach( 9);
 }
 
 void loop() {
